5336e6fa3b90bc5333a56cecac3dcc38e19db85d
Henrique Borges <henrique17_votu@hotmail.com>
2020-09-20 21:17:10 -0300

    Criação do projeto, primeiro commit

---
+++ src/main/java/com/dw2/rpingreact/HenriqueTrabalho/HenriqueTrabalhoApplication.java
@@ -0,0 +1,13 @@
+package com.dw2.rpingreact.HenriqueTrabalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HenriqueTrabalhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HenriqueTrabalhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dw2/rpingreact/HenriqueTrabalho/HenriqueTrabalhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dw2.rpingreact.HenriqueTrabalho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HenriqueTrabalhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
